9f300f44f411ad11e9c6f836ab6bdc22ff124369
Hadsan Geele <hgeele22@gmail.com>
2018-03-28 22:37:03 +0100

    first question

--- arrays_hashes_quiz.rb
+++ arrays_hashes_quiz.rb
@@ -1,149 +1,149 @@
-# Homework
-
-## Exercise A
-
-### Given the following data structure:
-
-stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
-
-### Complete these tasks:
-
-# 1. Add `"Edinburgh Waverley"` to the end of the array
-stops.push("Edinburgh Waverley")
-p stops
-# 2. Add `"Glasgow Queen St"` to the start of the array
-stops.unshift ("Glasgow Queen St")
-p stops
-# 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
-stops.insert(3, "Polmont")
-p stops
-# 4. Work out the index position of `"Linlithgow"`
-p stops.index("Linlithgow")
-
-# 5. Remove `"Livingston"` from the array using its name
-stops.delete("Livingston")
-p stops
-
-# 6. Delete `"Cumbernauld"` from the array by index
- stops.delete("Cumbernauld")
- p stops
-
-# 7. How many stops there are in the array?
-p stops.size
-
-# 8. How many ways can we return `"Falkirk High"` from the array?
-p stops[2]
-p stops [-4]
-
-# 9. Reverse the positions of the stops in the array
-p stops.reverse
-
-# 10. Print out all the stops using a for loop
-for station in stops
-  print station
-end
-
-
-## Exercise B
-
-### Given the following data structure:
-
-users = {
-  "Jonathan" => {
-    :twitter => "jonnyt",
-    :lottery_numbers => [6, 12, 49, 33, 45, 20],
-    :home_town => "Stirling",
-    :pets => [
-    {
-      :name => "fluffy",
-      :species => "cat"
-    },
-    {
-      :name => "fido",
-      :species => "dog"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    }
-  ]
-  },
-  "Erik" => {
-    :twitter => "eriksf",
-    :lottery_numbers => [18, 34, 8, 11, 24],
-    :home_town => "Linlithgow",
-    :pets => [
-    {
-      :name => "nemo",
-      :species => "fish"
-    },
-    {
-      :name => "kevin",
-      :species => "fish"
-    },
-    {
-      :name => "spike",
-      :species => "dog"
-    },
-    {
-      :name => "rupert",
-      :species => "parrot"
-    }
-  ]
-  },
-  "Avril" => {
-    :twitter => "bridgpally",
-    :lottery_numbers => [12, 14, 33, 38, 9, 25],
-    :home_town => "Dunbar",
-    :pets => [
-      {
-        :name => "monty",
-        :species => "snake"
-      }
-    ]
-  }
-}
-
-### Complete these tasks:
-
-1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
-p users["Jonathan"][:twitter]
-# 2. Get Erik's hometown
-p users["Erik"][:home_town]
-# 3. Get the array of Erik's lottery numbers
-users["Erik"][:lottery_numbers]
-# 4. Get the type of Avril's pet Monty
-users["Avril"][:name]
-# 5. Get the smallest of Erik's lottery numbers
-sorted_numbers= users["Erik"][:lottery_numbers].sort!
-p "The lowest number in Erik's lottery numbers was: #{sorted_numbers[0]}"
-
-# 6. Return an array of Avril's lottery numbers that are even
-avrils_lottery_number = users["Avril"][:lottery_numbers]
-even_number = []
-for num in avrils_lottery_number
-  if num %2 == 0
-    even_number << num
-  end
-end
-p even_number
-
-7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-p users["Erik"][:lottery_numbers] << 7
-
-# 8. Change Erik's hometown to Edinburgh
-p users['Erik'][:home_town] = "Edinburgh"
-
-
-# 9. Add a pet dog to Erik called "Fluffy"
-
-p users['Erik'][:pets] = "Fluffy"
-10. Add another person to the users hash
-
-user = { }
-p user[:name] = 'Mona'
-
-
+# # Homework
+#
+# ## Exercise A
+#
+# ### Given the following data structure:
+#
+# stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+#
+# ### Complete these tasks:
+#
+# # 1. Add `"Edinburgh Waverley"` to the end of the array
+# stops.push("Edinburgh Waverley")
+# p stops
+# # 2. Add `"Glasgow Queen St"` to the start of the array
+# stops.unshift ("Glasgow Queen St")
+# p stops
+# # 3. Add `"Polmont"` at the appropriate point (between `"Falkirk High"` and `"Linlithgow"`)
+# stops.insert(3, "Polmont")
+# p stops
+# # 4. Work out the index position of `"Linlithgow"`
+# p stops.index("Linlithgow")
+#
+# # 5. Remove `"Livingston"` from the array using its name
+# stops.delete("Livingston")
+# p stops
+#
+# # 6. Delete `"Cumbernauld"` from the array by index
+#  stops.delete("Cumbernauld")
+#  p stops
+#
+# # 7. How many stops there are in the array?
+# p stops.size
+#
+# # 8. How many ways can we return `"Falkirk High"` from the array?
+# p stops[2]
+# p stops [-4]
+#
+# # 9. Reverse the positions of the stops in the array
+# p stops.reverse
+#
+# # 10. Print out all the stops using a for loop
+# for station in stops
+#   print station
+# end
+#
+#
+# ## Exercise B
+#
+# ### Given the following data structure:
+#
+# users = {
+#   "Jonathan" => {
+#     :twitter => "jonnyt",
+#     :lottery_numbers => [6, 12, 49, 33, 45, 20],
+#     :home_town => "Stirling",
+#     :pets => [
+#     {
+#       :name => "fluffy",
+#       :species => "cat"
+#     },
+#     {
+#       :name => "fido",
+#       :species => "dog"
+#     },
+#     {
+#       :name => "spike",
+#       :species => "dog"
+#     }
+#   ]
+#   },
+#   "Erik" => {
+#     :twitter => "eriksf",
+#     :lottery_numbers => [18, 34, 8, 11, 24],
+#     :home_town => "Linlithgow",
+#     :pets => [
+#     {
+#       :name => "nemo",
+#       :species => "fish"
+#     },
+#     {
+#       :name => "kevin",
+#       :species => "fish"
+#     },
+#     {
+#       :name => "spike",
+#       :species => "dog"
+#     },
+#     {
+#       :name => "rupert",
+#       :species => "parrot"
+#     }
+#   ]
+#   },
+#   "Avril" => {
+#     :twitter => "bridgpally",
+#     :lottery_numbers => [12, 14, 33, 38, 9, 25],
+#     :home_town => "Dunbar",
+#     :pets => [
+#       {
+#         :name => "monty",
+#         :species => "snake"
+#       }
+#     ]
+#   }
+# }
+#
+# ### Complete these tasks:
+#
+# 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+# p users["Jonathan"][:twitter]
+# # 2. Get Erik's hometown
+# p users["Erik"][:home_town]
+# # 3. Get the array of Erik's lottery numbers
+# users["Erik"][:lottery_numbers]
+# # 4. Get the type of Avril's pet Monty
+# users["Avril"][:name]
+# # 5. Get the smallest of Erik's lottery numbers
+# sorted_numbers= users["Erik"][:lottery_numbers].sort!
+# p "The lowest number in Erik's lottery numbers was: #{sorted_numbers[0]}"
+#
+# # 6. Return an array of Avril's lottery numbers that are even
+# avrils_lottery_number = users["Avril"][:lottery_numbers]
+# even_number = []
+# for num in avrils_lottery_number
+#   if num %2 == 0
+#     even_number << num
+#   end
+# end
+# p even_number
+#
+# 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+# p users["Erik"][:lottery_numbers] << 7
+#
+# # 8. Change Erik's hometown to Edinburgh
+# p users['Erik'][:home_town] = "Edinburgh"
+#
+#
+# # 9. Add a pet dog to Erik called "Fluffy"
+#
+# p users['Erik'][:pets] = "Fluffy"
+# 10. Add another person to the users hash
+#
+# user = { }
+# p user[:name] = 'Mona'
+#
+#
 ## Exercise C
 
 ### Given the following data structure:
@@ -168,11 +168,28 @@ united_kingdom = [
 
 ## Complete these tasks:
 
-1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
- p united_kingdom[1][capital] = "Cardiff"
-# 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
-p united_kingdom.push ({ name:"Northern Ireland " , population:"1,811,000", capital: "Belfast" })
-3. Use a loop to print the names of all the countries in the UK.
+# 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
+#  p united_kingdom[1][capital] = "Cardiff"
+# # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
+# p united_kingdom.push ({ name:"Northern Ireland " , population:"1,811,000", capital: "Belfast" })
+# 3. Use a loop to print the names of all the countries in the UK.
+
+# name =["Scotland", "Wales", "England"]
+# name.each { |name| puts name }
+
+# 4. Use a loop to find the total population of the UK.
 
 
-4. Use a loop to find the total population of the UK.
+puts ":5295000 "
+first = gets.to_i
+puts ":3063000 "
+second = gets.to_i
+puts ":53010000 "
+third = gets.to_i
+
+total = 0
+for i in (first..third) do
+  total += i
+end
+
+puts total
